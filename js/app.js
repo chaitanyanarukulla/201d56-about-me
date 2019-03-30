bb2b8dc1ac20679e8d18e22b6764add576adb5ff
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-03-30 15:47:13 -0700

    updated question 6 and 7 with function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 'use strict';
 //Getting User name
+//Funtion to get username and check for  null and empty string and run loop untill name entered.
+function getUserName(){
+  let userName = null;
+  do {
+    userName = prompt('What is your name?');
+    console.log('User entered: ' + userName + ' for name.');
+    if (userName) {break;}
+    alert('Please enter a name!');
+  } while (userName === null || userName === '');
+  return userName;
+}
+
 function getToKnowMe() {/* function to ask questions when clicked on get to know me button on html */
   var userName = getUserName();
   alert('Greetings ' + userName + ' Lets play a game get to know Me');
   // Question 1
   var points = 0;
   var myCity = prompt(userName + ' do you think I am from Seattle');
-
   if(myCity ==='yes' || myCity === 'Yes' || myCity === 'y' || myCity === 'Y' || myCity === 'YES'){/* checking user
     answers with if else statements */
     points++;
@@ -21,10 +32,8 @@ function getToKnowMe() {/* function to ask questions when clicked on get to know
     alert('Must answer all the questions with yes or no');
     console.log('Must answer all the questions with yes or no');
   }
-
   //Question 2
   var myPreJob = prompt(userName + ' do you belive I fixed Airplanes in my previous Job');
-
   if(myPreJob ==='yes' || myPreJob === 'Yes' || myPreJob=== 'y' || myPreJob === 'Y'){/* checking user
     answers with if else statements */
     points++;
@@ -42,7 +51,6 @@ function getToKnowMe() {/* function to ask questions when clicked on get to know
 
   //Question 3
   var mySport = prompt(userName + ' do you think i play soccer').toUpperCase();
-
   if(mySport==='YES' || mySport === 'Y'){/* checking user
     answers with if else statements */
     points++;
@@ -57,10 +65,8 @@ function getToKnowMe() {/* function to ask questions when clicked on get to know
     alert('Must answer with yes or no');
     console.log('Must answer with yes or no');
   }
-
   //Question 4
   var myVetran = prompt(userName + ' do you think i am a Vetren').toLowerCase();
-
   if(myVetran === 'yes' || myVetran === 'y'){ /* checking user
     answers with if else statements */
     points++;
@@ -76,11 +82,8 @@ function getToKnowMe() {/* function to ask questions when clicked on get to know
     alert('Must answer with yes or no');
     console.log('Must answer with yes or no');
   }
-
-
   //Question 5
   var myFavFood = prompt(userName + ' do you belive i can speek 5 languages').toLowerCase();
-
   if(myFavFood === 'yes' || myFavFood === 'y'){/* checking user
     answers with if else statements */
     points++;
@@ -95,8 +98,7 @@ function getToKnowMe() {/* function to ask questions when clicked on get to know
     console.log('You have to answer with yes and no');
     alert('You have to answer with yes and no');
   }
-
-  //Question 7
+  //Question 6
   var stateIlived = prompt(userName + ' Can you guess a state that I have lived in besides Washington').toUpperCase();
   var states = ['VIRGINIA','CALIFORNIA','NEVADA','OREGON','IDAHO','COLORADO'];
   var attempts = 0;
@@ -116,64 +118,32 @@ function getToKnowMe() {/* function to ask questions when clicked on get to know
 
   }
 }
-//Question 6  Guessing Game
+
+//Question 7  Guessing Game
+function guessTheNumber() {
 // random value generated
-var randomNumber = Math.floor(Math.random() * 10 + 1);
-console.log('random number',randomNumber);
-
-// counting the number of guesses
-var guess = 0;
-
-document.getElementById('submitguess').onclick = function(){
-
+  var randomNumber = Math.floor(Math.random() * 15 + 1);
+  console.log('random number',randomNumber);
+  // counting the number of guesses
+  var guess = 0;
   // number guessed by user
-  var userGuessedNumber = document.getElementById('guessField').value;
-  console.log('user guessednumber'+userGuessedNumber);
-
+  var userGuessedNumber = prompt('We have selected a random number between 1 - 15. See if you can guess it in 4 Trys.');
   while(guess < 4){
-    console.log('we are guessing: ' + guess);
     if(randomNumber === parseInt(userGuessedNumber))
     {
       alert('CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN '+ guess + ' GUESS ');
       break;
-    } else if(randomNumber < parseInt(userGuessedNumber)) {/* if guessed number is greater
-                        than actual number*/
+    } else if(randomNumber < parseInt(userGuessedNumber)) {/* if guessed number is greater than actual number*/
       console.log('this is the random number',randomNumber);
       userGuessedNumber = prompt('OOPS SORRY!! TRY A SMALLER NUMBER');
       guess++;
-    } else if(randomNumber > parseInt(userGuessedNumber)){ /* if guessed number is Smaller
-        than actual number*/
+    } else if(randomNumber > parseInt(userGuessedNumber)){ /* if guessed number is Smaller than actual number*/
       userGuessedNumber = prompt('OOPS SORRY!! TRY A GREATER NUMBER');
       guess++;
     }
   }
-
-
   if(guess === 4){/* if number of tries is = 4 */
     alert('you failed to guess my number in 4 tryes');
   }
-
-};
-// function to hide and show guess number game on html
-
-function guessTheNumber() {
-  var guessGameB = document.getElementById('guessGameBlock');
-  if (guessGameB.style.display === 'none') {
-    guessGameB.style.display = 'block';
-  } else {
-    guessGameB.style.display = 'none';
-  }
 }
 
-//----
-//Funtion to get username and check for  null and empty string
-function getUserName(){
-  let userName = null;
-  do {
-    userName = prompt('What is your name?');
-    console.log('User entered: ' + userName + ' for name.');
-    if (userName) {break;}
-    alert('Please enter a name!');
-  } while (userName === null || userName === '');
-  return userName;
-}
